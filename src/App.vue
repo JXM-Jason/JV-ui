9daea5bf92f9e1d0f7fdad524ba7232644b591d4
JXM <1162782002@qq.com>
2022-04-14 10:55:16 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Jason></Jason>
+</template>
+
+<script>
+import Jason from "./components/Jason.vue"
+
+export default {
+  name: "App",
+  components: {
+    Jason,
+  },
+}
+</script>
---
+++ src/components/Jason.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="first">我是{{ this.msg }} 的第一个组件</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "JXM",
+    }
+  },
+}
+</script>
+
+<style lang="css">
+.first {
+  border: 1px solid red;
+}
+</style>
